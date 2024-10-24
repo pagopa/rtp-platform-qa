ba7460dae9a4207a8ad81d5d3b9905853b300ea8
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-10-24 16:24:55 +0200

    feat: [SRTP-45] add send RTP API test

--- functional-tests/tests/test_send_rtp.py
+++ functional-tests/tests/test_send_rtp.py
@@ -1,6 +1,9 @@
-import pytest
+from api.send_rtp import send_rtp
+from utils.dataset import generate_rtp_data
 
 
-@pytest.mark.need_fix
-def test_send_rtp():
-    assert True, 'This test should not fail'
+def test_send_rtp_api():
+    rtp_data = generate_rtp_data()
+
+    response = send_rtp(rtp_data)
+    assert response.status_code == 200
