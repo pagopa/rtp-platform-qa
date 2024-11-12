313a5501c3f712aecf0b31794c41379ee3aff308
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-11-12 11:24:43 +0100

    feat: add specification link to config

--- contract-tests/test_api_create_rtp.py
+++ contract-tests/test_api_create_rtp.py
@@ -5,7 +5,7 @@ from schemathesis import Case
 
 from config.configuration import config
 
-SPEC_URL = 'https://raw.githubusercontent.com/pagopa/rtp-apis/refs/heads/main/src/rtp/api/pagopa/openapi.yaml'
+SPEC_URL = config.rtp_api_specification
 BASE_URL = config.rtp_base_url_path
 
 schema = schemathesis.from_uri(SPEC_URL, base_url=BASE_URL)
