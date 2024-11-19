bd6037ba4379ff89845191c9fd187c8434e228fb
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-11-19 12:44:14 +0100

    fix: [SRTP-84] refactor activation contract tests

--- contract-tests/test_activation.py
+++ contract-tests/test_activation.py
@@ -1,8 +1,9 @@
 import schemathesis
 from schemathesis import Case
 
-from api.auth import get_valid_token
+from api.auth import get_valid_access_token
 from config.configuration import config
+from config.configuration import secrets
 
 SPEC_URL = config.activation_api_specification
 BASE_URL = config.rtp_base_url_path
@@ -12,15 +13,6 @@ schema = schemathesis.from_uri(SPEC_URL, base_url=BASE_URL)
 
 @schema.parametrize()
 def test_activation(case: Case):
-    if 'simulate_403' in case.path:
-        case.headers['Authorization'] = 'Bearer InvalidToken'
-    else:
-        valid_token = get_valid_token()
-        case.headers['Authorization'] = get_valid_token()
-
-    response = case.call_and_validate()
-
-    if case.headers['Authorization'] == 'Bearer InvalidToken':
-        assert response.status_code == 403
-    else:
-        response.raise_for_status()
+    access_token = get_valid_access_token(client_id=secrets.client_id,
+                                          client_secret=secrets.client_secret)
+    response = case.call_and_validate(headers={'Authorization': access_token})
