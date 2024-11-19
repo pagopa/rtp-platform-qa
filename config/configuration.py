b05e6ba5662dbb5d256f658897d7f0ece87337e2
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-11-19 11:39:54 +0100

    typo

--- config/configuration.py
+++ config/configuration.py
@@ -4,12 +4,12 @@ import logging
 
 from dynaconf import Dynaconf
 
-IDPAY_ENV_VAR_PREFIX = 'RTP'
+RTP_ENV_VAR_PREFIX = 'RTP'
 
 # `envvar_prefix` = export envvars with RTP_ENV_VAR_PREFIX as prefix.
 # `settings_files` = Load settings files in the order.
 config = Dynaconf(
-    envvar_prefix=IDPAY_ENV_VAR_PREFIX,
+    envvar_prefix=RTP_ENV_VAR_PREFIX,
     settings_files=['config.yaml'],
 )
 
