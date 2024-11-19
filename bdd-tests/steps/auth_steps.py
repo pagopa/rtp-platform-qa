d0513e679dc7e824a62b5c705067df5cdee8096a
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-11-19 12:11:10 +0100

    feat: [SRTP-94] add unhappy tests

--- bdd-tests/steps/auth_steps.py
+++ bdd-tests/steps/auth_steps.py
@@ -1,10 +1,10 @@
 from behave import given
 
-from api.auth import get_valid_token
+from api.auth import get_access_token
 from config.configuration import secrets
 
 
 @given('the Service Provider is authenticated')
 def given_ec_on_page(context):
-    access_token = get_valid_token(client_id=secrets.client_id, client_secret=secrets.client_secret)
+    access_token = get_access_token(client_id=secrets.client_id, client_secret=secrets.client_secret)
     context.token = access_token
--- functional-tests/tests/test_auth.py
+++ functional-tests/tests/test_auth.py
@@ -1,13 +1,32 @@
 import pytest
 
-from api.auth import get_valid_token
+from api.auth import get_access_token
+from api.auth import get_valid_access_token
 from config.configuration import secrets
 
 
 @pytest.mark.auth
 def test_get_valid_token():
-    token = get_valid_token(client_id=secrets.client_id, client_secret=secrets.client_secret)
+    token = get_valid_access_token(client_id=secrets.client_id, client_secret=secrets.client_secret)
 
     assert isinstance(token, str), 'Token must be a string'
     assert token.startswith('Bearer '), "Token must start with 'Bearer '"
     assert len(token) > 7, "Token should not be empty after 'Bearer '"
+
+
+@pytest.mark.auth
+def test_get_token_with_invalid_client_id():
+    invalid_client_id = '00000000-0000-0000-0000-000000000000'
+    token_response = get_access_token(client_id=invalid_client_id,
+                                      client_secret=secrets.client_secret)
+    assert token_response.status_code == 401
+    assert f'Client {invalid_client_id} not found' in str(token_response.json()['descriptions'])
+
+
+@pytest.mark.auth
+def test_get_token_with_invalid_client_secret():
+    invalid_client_secret = '000000000000000000000000000000000000'
+    token_response = get_access_token(client_id=secrets.client_id,
+                                      client_secret=invalid_client_secret)
+    assert token_response.status_code == 401
+    assert 'Wrong secret' in str(token_response.json()['descriptions'])
