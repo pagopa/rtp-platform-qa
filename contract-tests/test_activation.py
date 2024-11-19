0f94a0b4f95351c9bdc957bfa810e73035a4f4fe
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-11-19 10:26:14 +0100

    feat: [SRTP-84] add activation contract test

--- contract-tests/test_activation.py
+++ contract-tests/test_activation.py
@@ -1,6 +1,7 @@
 import schemathesis
 from schemathesis import Case
 
+from api.auth import get_valid_token
 from config.configuration import config
 
 SPEC_URL = config.activation_api_specification
@@ -11,4 +12,15 @@ schema = schemathesis.from_uri(SPEC_URL, base_url=BASE_URL)
 
 @schema.parametrize()
 def test_activation(case: Case):
-    case.call_and_validate()
+    if 'simulate_403' in case.path:
+        case.headers['Authorization'] = 'Bearer InvalidToken'
+    else:
+        valid_token = get_valid_token()
+        case.headers['Authorization'] = get_valid_token()
+
+    response = case.call_and_validate()
+
+    if case.headers['Authorization'] == 'Bearer InvalidToken':
+        assert response.status_code == 403
+    else:
+        response.raise_for_status()
