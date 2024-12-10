c13b1277633c26bcb96eec729a697e667559c031
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-12-10 17:42:57 +0100

    feat: [SRTP-135] add send RTP through API steps

--- bdd-tests/steps/send_rtp_steps.py
+++ bdd-tests/steps/send_rtp_steps.py
@@ -2,6 +2,21 @@ from behave import given
 from behave import then
 from behave import when
 
+from api.send_rtp import send_rtp
+from utils.dataset import generate_rtp_data
+
+
+@when('the Service Provider sends an RTP to the debtor {debtor_name}')
+def when_sp_sends_rtp(context, debtor_name: str):
+    debtor_fc = context.debtor_fc[debtor_name]
+    res = send_rtp(access_token=context.access_token, rtp_payload=generate_rtp_data(payer_id=debtor_fc))
+    context.latest_rtp_response = res
+
+
+@then('the RTP is created correctly')
+def then_the_rtp_is_delivered(context):
+    assert context.latest_rtp_response.status_code == 201
+
 
 @given('the Ente Creditore is on the web page')
 def given_ec_on_page(context):
