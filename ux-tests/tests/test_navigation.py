3031cfa979f463edf2074807ae82d992bd153b82
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-12-16 17:32:41 +0100

    feat: [SRTP-158] add Allure and Pytest tags to tests

--- ux-tests/tests/test_navigation.py
+++ ux-tests/tests/test_navigation.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import allure
 import pytest
 from playwright.sync_api import expect
 from playwright.sync_api import sync_playwright
@@ -25,6 +26,9 @@ def page(playwright_browser):
     page.close()
 
 
+@allure.feature('RTP send')
+@allure.story('RTP submission though web page')
+@allure.title('RTP form is filled and submitted')
 def test_rtp_form_submission(page):
     rtp_data = generate_rtp_data()
 
@@ -33,7 +37,8 @@ def test_rtp_form_submission(page):
     page.fill('input[id="noticeNumber"]', rtp_data['noticeNumber'])
     page.fill('input[id="amount"]', str(rtp_data['amount']))
     page.fill('input[id="description"]', rtp_data['description'])
-    page.fill('input[placeholder="DD/MM/YYYY"]', str(datetime.strptime(rtp_data['expiryDate'], '%Y-%m-%d').strftime('%d/%m/%Y')))
+    page.fill('input[placeholder="DD/MM/YYYY"]',
+              str(datetime.strptime(rtp_data['expiryDate'], '%Y-%m-%d').strftime('%d/%m/%Y')))
     page.fill('input[id="payeeCompanyName"]', rtp_data['payee']['name'])
     page.fill('input[id="payee"]', rtp_data['payee']['payeeId'])
     page.fill('input[id="payerId"]', rtp_data['payerId'])
@@ -45,6 +50,9 @@ def test_rtp_form_submission(page):
     expect(popup_message).to_have_text('Request to pay created successfully!')
 
 
+@allure.feature('RTP Submission')
+@allure.story('Input validation')
+@allure.title('Comma is not allowed in amount field')
 def test_comma_not_allowed(page):
     rtp_data = generate_rtp_data()
 
@@ -66,6 +74,9 @@ def test_comma_not_allowed(page):
     expect(popup_message).not_to_be_visible()
 
 
+@allure.feature('RTP Submission')
+@allure.story('Input validation')
+@allure.title('Whitespaces is allowed in description and company name field')
 def test_whitespace_allowed_in_description_and_payee_company_name(page):
     page.goto(page_url)
 
