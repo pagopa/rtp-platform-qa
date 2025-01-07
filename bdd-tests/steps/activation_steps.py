97b69903cbb19b72c3c0b798befd0d2532a9d6bb
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-01-07 16:26:12 +0100

    fix activate assertion

--- bdd-tests/steps/activation_steps.py
+++ bdd-tests/steps/activation_steps.py
@@ -11,21 +11,22 @@ from config.configuration import secrets
 def given_sp_activates_debtor(context, debtor_name):
     debtor_fc = context.debtor_fc[debtor_name]
     res = activate(context.access_token, debtor_fc, secrets.service_provider_id)
-    assert res.status_code == 201
+
+    assert res.status_code == 201, f'Activation failed with status code {res.status_code}'
 
 
 @when('the Service Provider tries to activate the debtor {debtor_name}')
 def given_sp_tries_to_activate_debtor(context, debtor_name):
     debtor_fc = context.debtor_fc[debtor_name]
     activation_response = activate(context.access_token, debtor_fc, secrets.service_provider_id)
-    context.activation_response = activation_response
+    context.latest_activation_response = activation_response
 
 
 @then('the activation fails because the Service Provider has wrong credentials')
 def then_sp_fails_activation_wrong_credentials(context):
-    assert context.activation_response.status_code == 401
+    assert context.latest_activation_response.status_code == 401
 
 
 @then('the activation fails because debtor {debtor_name} is already activated')
 def then_sp_fails_activation_debtor_already_activate(context, debtor_name):
-    assert context.activation_response == 409
+    assert context.latest_activation_response.status_code == 409
