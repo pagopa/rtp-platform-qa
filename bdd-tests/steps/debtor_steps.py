5de4a7568e3a1261f42f967bb118bfe2d00d40e5
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-01-09 15:31:17 +0100

    feat: [SRTP-189] add get activation to existing tests

--- bdd-tests/steps/debtor_steps.py
+++ bdd-tests/steps/debtor_steps.py
@@ -1,11 +1,6 @@
 from behave import then
 
 
-@then('the debtor {debtor_name} is activated')
-def then_the_debtor_is_activated(context, debtor_name):
-    pass
-
-
 @then('the debtor {debtor_name} receives the RTP')
 def then_the_rtp_is_created(context, debtor_name):
     pass
