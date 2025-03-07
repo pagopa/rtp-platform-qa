bcf20a8c17bd007d3b628a2aa7d513979406a929
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-03-07 16:27:02 +0100

    feat: [SRTP-306] add CBI RTP data generation function and update requirements

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,10 +1,13 @@
+import math
 import random
 import re
 import string
+import uuid
 from datetime import datetime
 from datetime import timedelta
 
 from faker import Faker
+from schwifty import IBAN
 
 fake = Faker('it_IT')
 
@@ -12,7 +15,8 @@ TEST_PAYEE_COMPANY_NAME = 'Test payee company name'
 
 uuidv4_pattern = re.compile(r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}')
 
-def generate_rtp_data(payer_id: str = '' ):
+
+def generate_rtp_data(payer_id: str = ''):
     notice_number = ''.join([str(random.randint(0, 9)) for _ in range(18)])
 
     amount = round(random.uniform(0, 999999999), 2)
@@ -43,7 +47,6 @@ def generate_rtp_data(payer_id: str = '' ):
         'expiryDate': expiry_date,
     }
 
-
     return {
         'payee': payee,
         'payer': payer,
@@ -68,7 +71,7 @@ def fake_fc(age: int = None, custom_month: int = None, custom_day: int = None, s
     checksum = fake_cf[15]
 
     if age is not None:
-        year = (datetime.datetime.now() - datetime.timedelta(days=int(age) * 365)).strftime('%Y')[2:]
+        year = (datetime.now() - timedelta(days=int(age) * 365)).strftime('%Y')[2:]
 
     if custom_month is not None and 1 <= custom_month <= 12:
         month_letter = month_number_to_fc_letter(custom_month)
@@ -94,3 +97,113 @@ def month_number_to_fc_letter(month_num):
         return months[int(month_num) - 1]
     else:
         return 'A'
+
+
+def generate_cbi_rtp_data(rtp_data: dict = None) -> dict:
+    """Generates CBI-compliant RTP payload
+    :param rtp_data: Optional RTP data to base the CBI payload on
+    :returns: Dictionary containing CBI-compliant RTP payload
+    """
+    if not rtp_data:
+        rtp_data = generate_rtp_data()
+
+    resource_id = str(uuid.uuid4())
+
+    return {
+        'resourceId': resource_id,
+        'Document': {
+            'CdtrPmtActvtnReq': {
+                'GrpHdr': {
+                    'MsgId': resource_id,
+                    'CreDtTm': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f'),
+                    'NbOfTxs': '1',
+                    'InitgPty': {
+                        'Nm': 'PagoPA'
+                    }
+                },
+                'PmtInf': [{
+                    'PmtInfId': rtp_data['paymentNotice']['noticeNumber'],
+                    'PmtMtd': 'TRF',
+                    'ReqdExctnDt': {
+                        'Dt': rtp_data['paymentNotice']['expiryDate']
+                    },
+                    'XpryDt': {
+                        'Dt': rtp_data['paymentNotice']['expiryDate']
+                    },
+                    'Dbtr': {
+                        'Nm': rtp_data['payer']['name'],
+                        'Id': {
+                            'PrvtId': {
+                                'Othr': [{
+                                    'Id': rtp_data['payer']['payerId'],
+                                    'SchmeNm': {
+                                        'Cd': 'POID'
+                                    }
+                                }]
+                            }
+                        }
+                    },
+                    'DbtrAgt': {
+                        'FinInstnId': {
+                            'BICFI': 'FAKESP01'
+                        }
+                    },
+                    'CdtTrfTx': [{
+                        'PmtId': {
+                            'InstrId': resource_id,
+                            'EndToEndId': rtp_data['paymentNotice']['noticeNumber']
+                        },
+                        'PmtTpInf': {
+                            'SvcLvl': {
+                                'Cd': 'SRTP'
+                            },
+                            'LclInstrm': {
+                                'Prtry': 'PAGOPA'
+                            }
+                        },
+                        'Amt': {
+                            'InstdAmt': rtp_data['paymentNotice']['amount']
+                        },
+                        'ChrgBr': 'SLEV',
+                        'CdtrAgt': {
+                            'FinInstnId': {
+                                'Othr': {
+                                    'Id': rtp_data['payee']['payeeId'],
+                                    'SchmeNm': {
+                                        'Cd': 'BOID'
+                                    }
+                                }
+                            }
+                        },
+                        'Cdtr': {
+                            'Nm': rtp_data['payee']['name'],
+                            'Id': {
+                                'OrgId': {
+                                    'Othr': [{
+                                        'Id': rtp_data['payee']['payeeId'],
+                                        'SchmeNm': {
+                                            'Cd': 'BOID'
+                                        }
+                                    }]
+                                }
+                            }
+                        },
+                        'CdtrAcct': {
+                            'Id': {
+                                'IBAN': IBAN.generate('IT', bank_code='00000', account_code=str(
+                                    round(random.random() * math.pow(10, 10))) + '99').compact
+                            }
+                        },
+                        'InstrForCdtrAgt': [{
+                            'InstrInf': rtp_data['payee']['payTrxRef']
+                        }],
+                        'RmtInf': {
+                            'Ustrd': [rtp_data['paymentNotice']['description']]
+                        },
+                        'NclsdFile': []
+                    }]
+                }]
+            }
+        },
+        'callbackUrl': 'http://spsrtp.api.uat.cstar.pagopa.it'
+    }
