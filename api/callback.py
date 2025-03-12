e2167211d6fae0e2e732cd903b15bbd745cc869d
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-03-12 12:45:45 +0100

    feat: [SRTP-322] enhance srtp_callback to accept certificate and key paths for secure RTP handling

--- api/callback.py
+++ api/callback.py
@@ -3,8 +3,12 @@ import requests
 from config.configuration import config
 
 
-def srtp_callback(rtp_payload):
+def srtp_callback(cert_path: str, key_path: str, rtp_payload):
     return requests.post(
+        cert=(
+            cert_path,
+            key_path
+        ),
         url=config.callback_url,
         json=rtp_payload,
         timeout=config.default_timeout
