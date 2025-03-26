255ef287084fd394cf94f120d1839e02f026480a
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-03-26 15:56:31 +0100

    feat: add assertion on successful send and remove useless test

--- ux-tests/tests/test_navigation.py
+++ ux-tests/tests/test_navigation.py
@@ -1,6 +1,8 @@
+import re
 from datetime import datetime
 
 import allure
+import pyperclip
 import pytest
 from playwright.sync_api import expect
 from playwright.sync_api import sync_playwright
@@ -51,54 +53,50 @@ def test_rtp_form_submission(page):
 
     page.fill('input[name="paymentNotice.noticeNumber"]', rtp_data['paymentNotice']['noticeNumber'])
     page.fill('input[name="paymentNotice.amount"]', str(rtp_data['paymentNotice']['amount']))
-    page.fill('input[placeholder="DD/MM/YYYY"]', str(datetime.strptime(rtp_data['paymentNotice']['expiryDate'], '%Y-%m-%d').strftime('%d/%m/%Y')))
+    page.fill('input[placeholder="DD/MM/YYYY"]',
+              str(datetime.strptime(rtp_data['paymentNotice']['expiryDate'], '%Y-%m-%d').strftime('%d/%m/%Y')))
     page.fill('input[name="paymentNotice.subject"]', rtp_data['paymentNotice']['description'])
     page.fill('input[name="paymentNotice.description"]', rtp_data['paymentNotice']['description'])
     page.click('[type=submit]')
 
-    popup_message = page.locator("div[role='dialog']")
-    expect(popup_message).to_have_text('Richiesta di pagamento inviata correttamente')
-    # popup_message = page.locator("div[role='alert']")
-    # expect(popup_message).to_have_text('Errore durante la creazione della richiesta di pagamento')
+    # Check success message and URL
+    message = page.locator('text=La richiesta è stata inviata con successo!')
+    expect(message).to_be_visible()
+    base_url = re.escape(config.landing_page_path.split('/')[0] + '//' + config.landing_page_path.split('/')[2])
+    uuidv4_pattern = f'[0-9a-f]{{8}}-[0-9a-f]{{4}}-4[0-9a-f]{{3}}-[89ab][0-9a-f]{{3}}-[0-9a-f]{{12}}'
+    assert (re.search(f'{base_url}/{uuidv4_pattern}/ok', page.url))
+    resource_id_from_url = re.search(uuidv4_pattern, page.url).group(0)
+    resource_id = page.locator(f'text={resource_id_from_url}')
+    expect(resource_id).to_be_visible()
 
-
-@allure.feature('RTP Submission')
-@allure.story('Input validation')
-@allure.title('Comma is not allowed in amount field')
-def test_comma_not_allowed(page):
-    rtp_data = generate_rtp_data()
-
-    page.goto(page_url)
-
-    page.fill('input[id="noticeNumber"]', rtp_data['paymentNotice']['noticeNumber'])
-    page.fill('input[id="amount"]', str(rtp_data['paymentNotice']['amount']).replace('.', ','))
-    page.fill('input[id="description"]', rtp_data['paymentNotice']['description'])
-    page.fill('input[placeholder="DD/MM/YYYY"]', rtp_data['paymentNotice']['expiryDate'])
-    page.fill('input[id="payeeCompanyName"]', rtp_data['payee']['name'])
-    page.fill('input[id="payee"]', rtp_data['payee']['payeeId'])
-    page.fill('input[id="payerId"]', rtp_data['payer']['payerId'])
-
-    validation_message = page.locator('#amount-helper-text')
-    expect(validation_message).to_have_text('Enter a valid number. To separate decimals use the dot (.)')
-
-    page.click('button[id="paymentNoticeButtonContinue"]')
-    popup_message = page.locator("div[role='dialog'] p.MuiDialogContentText-root")
-    expect(popup_message).not_to_be_visible()
+    # Check if the resource id is copied to the clipboard
+    copy_button = page.locator('button[aria-label="Copia"]')
+    copy_button.click()
+    page.wait_for_timeout(500)
+    clipboard_text = pyperclip.paste()
+    assert clipboard_text == resource_id_from_url
 
 
 @allure.feature('RTP Submission')
 @allure.story('Input validation')
 @allure.title('Whitespaces is allowed in description and company name field')
-def test_whitespace_allowed_in_description_and_payee_company_name(page):
+def test_whitespace_and_capitalization_in_description_and_payee_company_name(page):
     page.goto(page_url)
+    expect(page).to_have_url(config.login_page_path)
+
+    page.fill('input[name="username"]', secrets.webpage.username)
+    page.fill('input[name="password"]', secrets.webpage.password)
+    page.click('button[id=":r2:"]')
+
+    expect(page).to_have_url(page_url)
 
     description = 'Description with spaces'
     payee_company_name = 'Payee company name with spaces'
 
-    page.fill('input[id="description"]', description)
-    filled_description = page.locator('input[id="description"]')
-    assert filled_description.input_value() == description
+    page.fill('input[name="paymentNotice.description"]', description)
+    filled_description = page.locator('input[name="paymentNotice.description"]')
+    assert filled_description.input_value() == description.upper()
 
-    page.fill('input[id="payeeCompanyName"]', payee_company_name)
-    filled_description = page.locator('input[id="payeeCompanyName"]')
-    assert filled_description.input_value() == payee_company_name
+    page.fill('input[name="payee.name"]', payee_company_name)
+    filled_description = page.locator('input[name="payee.name"]')
+    assert filled_description.input_value() == payee_company_name.upper()
