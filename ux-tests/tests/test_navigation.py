a02d2db072e6ad6815978d8dba2bedda9bce8c32
Tommaso Lencioni <tommaso.lencioni@pagopa.it>
2025-04-04 10:27:39 +0200

    feat: Add login to webpage (#28)
    * feat: refactor form submission adding login
    
    * fix: fix submission and add commented assertions
    
    * feat: add assertion on successful send and remove useless test
    
    * fix: pre-commit fix
    
    * remove clipboard check
    
    * mock fiscal code
    
    * fix: remove unused pyperclip import

--- ux-tests/tests/test_navigation.py
+++ ux-tests/tests/test_navigation.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 
 import allure
@@ -6,6 +7,7 @@ from playwright.sync_api import expect
 from playwright.sync_api import sync_playwright
 
 from config.configuration import config
+from config.configuration import secrets
 from utils.dataset import generate_rtp_data
 
 page_url = config.landing_page_path
@@ -31,61 +33,63 @@ def page(playwright_browser):
 @allure.title('RTP form is filled and submitted')
 def test_rtp_form_submission(page):
     rtp_data = generate_rtp_data()
+    rtp_data['payer']['payerId'] = 'CPPRML60T46X000R'
 
     page.goto(page_url)
+    expect(page).to_have_url(config.login_page_path)
 
-    page.fill('input[id="noticeNumber"]', rtp_data['paymentNotice']['noticeNumber'])
-    page.fill('input[id="amount"]', str(rtp_data['paymentNotice']['amount']))
-    page.fill('input[id="description"]', rtp_data['paymentNotice']['description'])
-    page.fill('input[placeholder="DD/MM/YYYY"]', str(datetime.strptime(rtp_data['paymentNotice']['expiryDate'], '%Y-%m-%d').strftime('%d/%m/%Y')))
-    page.fill('input[id="payeeCompanyName"]', rtp_data['payee']['name'])
-    page.fill('input[id="payee"]', rtp_data['payee']['payeeId'])
-    page.fill('input[id="payerId"]', rtp_data['payer']['payerId'])
+    page.fill('input[name="username"]', secrets.webpage.username)
+    page.fill('input[name="password"]', secrets.webpage.password)
+    page.click('button[id=":r2:"]')
 
-    page.click('button[id="paymentNoticeButtonContinue"]')
+    expect(page).to_have_url(page_url)
 
-    popup_message = page.locator("div[role='dialog'] p.MuiDialogContentText-root")
-    expect(popup_message).to_be_visible()
-    expect(popup_message).to_have_text('Request to pay created successfully!')
+    page.fill('input[name="payee.name"]', rtp_data['payee']['name'])
+    page.fill('input[name="payee.payeeId"]', rtp_data['payee']['payeeId'])
+    page.fill('input[name="payee.payTrxRef"]', rtp_data['paymentNotice']['noticeNumber'])
 
+    page.fill('input[name="payer.name"]', rtp_data['payer']['payerId'])
+    page.fill('input[name="payer.payerId"]', rtp_data['payer']['payerId'])
 
-@allure.feature('RTP Submission')
-@allure.story('Input validation')
-@allure.title('Comma is not allowed in amount field')
-def test_comma_not_allowed(page):
-    rtp_data = generate_rtp_data()
+    page.fill('input[name="paymentNotice.noticeNumber"]', rtp_data['paymentNotice']['noticeNumber'])
+    page.fill('input[name="paymentNotice.amount"]', str(rtp_data['paymentNotice']['amount']))
+    page.fill('input[placeholder="DD/MM/YYYY"]',
+              str(datetime.strptime(rtp_data['paymentNotice']['expiryDate'], '%Y-%m-%d').strftime('%d/%m/%Y')))
+    page.fill('input[name="paymentNotice.subject"]', rtp_data['paymentNotice']['description'])
+    page.fill('input[name="paymentNotice.description"]', rtp_data['paymentNotice']['description'])
+    page.click('[type=submit]')
 
-    page.goto(page_url)
-
-    page.fill('input[id="noticeNumber"]', rtp_data['paymentNotice']['noticeNumber'])
-    page.fill('input[id="amount"]', str(rtp_data['paymentNotice']['amount']).replace('.', ','))
-    page.fill('input[id="description"]', rtp_data['paymentNotice']['description'])
-    page.fill('input[placeholder="DD/MM/YYYY"]', rtp_data['paymentNotice']['expiryDate'])
-    page.fill('input[id="payeeCompanyName"]', rtp_data['payee']['name'])
-    page.fill('input[id="payee"]', rtp_data['payee']['payeeId'])
-    page.fill('input[id="payerId"]', rtp_data['payer']['payerId'])
-
-    validation_message = page.locator('#amount-helper-text')
-    expect(validation_message).to_have_text('Enter a valid number. To separate decimals use the dot (.)')
-
-    page.click('button[id="paymentNoticeButtonContinue"]')
-    popup_message = page.locator("div[role='dialog'] p.MuiDialogContentText-root")
-    expect(popup_message).not_to_be_visible()
+    # Check success message and URL
+    message = page.locator('text=La richiesta è stata inviata con successo!')
+    expect(message).to_be_visible()
+    base_url = re.escape(config.landing_page_path.split('/')[0] + '//' + config.landing_page_path.split('/')[2])
+    uuidv4_pattern = f'[0-9a-f]{{8}}-[0-9a-f]{{4}}-4[0-9a-f]{{3}}-[89ab][0-9a-f]{{3}}-[0-9a-f]{{12}}'
+    assert (re.search(f'{base_url}/{uuidv4_pattern}/ok', page.url))
+    resource_id_from_url = re.search(uuidv4_pattern, page.url).group(0)
+    resource_id = page.locator(f'text={resource_id_from_url}')
+    expect(resource_id).to_be_visible()
 
 
 @allure.feature('RTP Submission')
 @allure.story('Input validation')
 @allure.title('Whitespaces is allowed in description and company name field')
-def test_whitespace_allowed_in_description_and_payee_company_name(page):
+def test_whitespace_and_capitalization_in_description_and_payee_company_name(page):
     page.goto(page_url)
+    expect(page).to_have_url(config.login_page_path)
+
+    page.fill('input[name="username"]', secrets.webpage.username)
+    page.fill('input[name="password"]', secrets.webpage.password)
+    page.click('button[id=":r2:"]')
+
+    expect(page).to_have_url(page_url)
 
     description = 'Description with spaces'
     payee_company_name = 'Payee company name with spaces'
 
-    page.fill('input[id="description"]', description)
-    filled_description = page.locator('input[id="description"]')
-    assert filled_description.input_value() == description
+    page.fill('input[name="paymentNotice.description"]', description)
+    filled_description = page.locator('input[name="paymentNotice.description"]')
+    assert filled_description.input_value() == description.upper()
 
-    page.fill('input[id="payeeCompanyName"]', payee_company_name)
-    filled_description = page.locator('input[id="payeeCompanyName"]')
-    assert filled_description.input_value() == payee_company_name
+    page.fill('input[name="payee.name"]', payee_company_name)
+    filled_description = page.locator('input[name="payee.name"]')
+    assert filled_description.input_value() == payee_company_name.upper()
