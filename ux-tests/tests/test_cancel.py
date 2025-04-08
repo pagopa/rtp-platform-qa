419261db551a85dc73fcecaab8e17c836ea64d79
Michele Fucito <michele.fucito@digitouch.it>
2025-04-08 15:06:26 +0200

    feat: [SRTP-340] add test for cancel after request

---
+++ ux-tests/tests/test_cancel.py
@@ -0,0 +1,23 @@
+import allure
+from playwright.sync_api import expect
+from test_submission import test_rtp_form_submission
+
+
+@allure.feature('RTP Submission')
+@allure.story('RTP cancellation through web page')
+@allure.title('RTP form cancellation test')
+def test_rtp_form_cancellation(page):
+    test_rtp_form_submission(page)
+
+    page.click('button:has-text("Cancella richiesta")')
+
+    modal = page.get_by_role("dialog", name="Vuoi cancellare la richiesta?")
+    expect(modal).to_be_visible()
+
+    expect(page.locator('text=Vuoi cancellare la richiesta?')).to_be_visible()
+    expect(page.locator('text=Proseguendo, la richiesta di pagamento appena creata verrà cancellata.')).to_be_visible()
+
+    modal.locator('button:has-text("Cancella richiesta")').click()
+
+    cancellation_msg = page.locator('text=La richiesta è stata cancellata')
+    expect(cancellation_msg).to_be_visible()
