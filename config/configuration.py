2cb60db8a0219d0214a7facfde11b7abbf734ad4
Michele Fucito <michele.fucito@digitouch.it>
2025-05-28 11:45:24 +0200

    Revert "fix: [SRTP-533] update dynaconf to manage secrets correctly"
    This reverts commit a4f29c7e36c129cb13852717f76bb318b528e4e9.

--- config/configuration.py
+++ config/configuration.py
@@ -1,5 +1,5 @@
-"""Parse configuration file to obtain current settings."""
-
+"""Parse configuration file to obtain current settings.
+"""
 import logging
 
 from dynaconf import Dynaconf
@@ -8,27 +8,24 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent.resolve()
 
 
-RTP_ENV_VAR_PREFIX = "RTP"
+RTP_ENV_VAR_PREFIX = 'RTP'
 
 # `envvar_prefix` = export envvars with RTP_ENV_VAR_PREFIX as prefix.
 # `settings_files` = Load settings files in the order.
 config = Dynaconf(
     envvar_prefix=RTP_ENV_VAR_PREFIX,
-    settings_files=["config.yaml"],
+    settings_files=['config.yaml'],
 )
 config.cert_path = str(BASE_DIR / config.cert_path)
-config.key_path = str(BASE_DIR / config.key_path)
+config.key_path  = str(BASE_DIR / config.key_path)
 
 # Load the secrets for the specified environment
 secrets = {}
 
 try:
-    all_secrets = Dynaconf(
-        settings_files=config.SECRET_PATH,
-        environments=True,
-        env_switcher="TARGET_ENV",
-        default_env="uat",
-    )
+    all_secrets = Dynaconf(settings_files=config.SECRET_PATH)
+    if config.TARGET_ENV in all_secrets:
+        secrets = all_secrets[config.TARGET_ENV]
 except AttributeError as e:
     logging.error(e)
     exit()
