1238cf2c44271de6529a9b6d4d13aaf4b1277a3c
Michele Fucito <michele.fucito@digitouch.it>
2025-07-01 10:05:40 +0200

    feat [SRTP-627]: add unhappy path for poste availability

--- functional-tests/tests/test_debtor_service_provider_availability.py
+++ functional-tests/tests/test_debtor_service_provider_availability.py
@@ -123,3 +123,46 @@ def test_send_rtp_to_poste():
     response = send_srtp_to_poste(poste_payload)
 
     assert response.status_code == 201
+
+
+@allure.feature('RTP Send')
+@allure.story('Service provider sends an RTP to POSTE')
+@allure.title('Cannot send RTP with invalid amount')
+@pytest.mark.send
+@pytest.mark.unhappy_path
+@pytest.mark.poste
+def test_send_rtp_to_poste_invalid_amount():
+    rtp_data = generate_rtp_data()
+    rtp_data['paymentNotice']['amount'] = -1
+    poste_payload = generate_epc_rtp_data(rtp_data)
+    response = send_srtp_to_poste(poste_payload)
+
+    assert response.status_code == 400
+
+@allure.feature('RTP Send')
+@allure.story('Service provider sends an RTP to POSTE')
+@allure.title('Cannot send RTP with an amount over the POSTE limit')
+@pytest.mark.send
+@pytest.mark.unhappy_path
+@pytest.mark.poste
+def test_send_rtp_to_poste_over_limit_amount():
+    rtp_data = generate_rtp_data()
+    poste_payload = generate_epc_rtp_data(rtp_data)
+    response = send_srtp_to_poste(poste_payload)
+
+    assert response.status_code == 400
+
+
+@allure.feature('RTP Send')
+@allure.story('Service provider sends an RTP to POSTE')
+@allure.title('Cannot send RTP with expired date')
+@pytest.mark.send
+@pytest.mark.unhappy_path
+@pytest.mark.poste
+def test_send_rtp_to_poste_expired_date():
+    rtp_data = generate_rtp_data()
+    rtp_data['paymentNotice']['expiryDate'] = '2020-01-01'
+    poste_payload = generate_epc_rtp_data(rtp_data)
+
+    response = send_srtp_to_poste(poste_payload)
+    assert response.status_code == 400
