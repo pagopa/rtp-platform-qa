a779126030b2d1c31819302e0866b022182a6db4
Michele Fucito <michele.fucito@digitouch.it>
2025-07-24 15:01:56 +0200

    feat [SRTP-581]: add get after create and update to check if updated succeded

--- functional-tests/tests/test_debt_position.py
+++ functional-tests/tests/test_debt_position.py
@@ -1,6 +1,7 @@
+import time
+
 import allure
 import pytest
-import time
 
 from api.activation import activate
 from api.auth import get_access_token
@@ -9,12 +10,13 @@ from api.debt_position import create_debt_position
 from api.debt_position import create_debt_position_dev
 from api.debt_position import delete_debt_position
 from api.debt_position import delete_debt_position_dev
-from api.debt_position import update_debt_position
-from api.debt_position import update_debt_position_dev
 from api.debt_position import get_debt_position
 from api.debt_position import get_debt_position_dev
+from api.debt_position import update_debt_position
+from api.debt_position import update_debt_position_dev
 from config.configuration import secrets
-from utils.dataset import create_debt_position_payload, create_debt_position_update_payload
+from utils.dataset import create_debt_position_payload
+from utils.dataset import create_debt_position_update_payload
 from utils.dataset import fake_fc
 from utils.dataset import generate_iupd
 from utils.dataset import generate_iuv
@@ -127,8 +129,8 @@ def test_delete_debt_position_happy_path():
     delete_response = delete_debt_position(subscription_key, organization_id, iupd)
     assert delete_response.status_code == 200, f'Expected 200 but got {delete_response.status_code}'
 
-    # get_response = get_debt_position(subscription_key, organization_id, iupd)
-    # assert get_response.status_code == 200, f'Expected 200 but got {get_response.status_code}'
+    get_response = get_debt_position(subscription_key, organization_id, iupd)
+    assert get_response.status_code == 404, f'Expected 404 but got {get_response.status_code}'
 
 @allure.feature('Debt Positions')
 @allure.story('Delete Debt Position')
@@ -168,8 +170,8 @@ def test_delete_debt_position_dev_happy_path():
     delete_response = delete_debt_position_dev(subscription_key, organization_id, iupd)
     assert delete_response.status_code == 200, f'Expected 200 but got {delete_response.status_code}'
 
-    # get_response = get_debt_position(subscription_key, organization_id, iupd)
-    # assert get_response.status_code == 200, f'Expected 200 but got {get_response.status_code}'
+    get_response = get_debt_position(subscription_key, organization_id, iupd)
+    assert get_response.status_code == 404, f'Expected 404 but got {get_response.status_code}'
 
 
 @allure.feature('Debt Positions')
@@ -183,9 +185,9 @@ def test_update_debt_position_happy_path():
         client_secret=secrets.debtor_service_provider.client_secret,
         access_token_function=get_access_token
     )
-    
+
     debtor_fc = fake_fc()
-    
+
     activation_response = activate(
         access_token,
         debtor_fc,
@@ -203,14 +205,30 @@ def test_update_debt_position_happy_path():
 
     create_response = create_debt_position(subscription_key, organization_id, payload, to_publish=True)
     assert create_response.status_code == 201, f'Expected 201 but got {create_response.status_code}'
-    
+
+    get_initial_response = get_debt_position(subscription_key, organization_id, iupd)
+    assert get_initial_response.status_code == 200, f'Expected 200 but got {get_initial_response.status_code}'
+    initial_data = get_initial_response.json()
+    initial_description = initial_data['paymentOption'][0]['description']
+    initial_amount = initial_data['paymentOption'][0]['amount']
+
     time.sleep(1)
-    
+
     update_payload = create_debt_position_update_payload(iupd=iupd, debtor_fc=debtor_fc, iuv=iuv)
-    
     update_response = update_debt_position(subscription_key, organization_id, iupd, update_payload)
     assert update_response.status_code == 200, f'Expected 200 but got {update_response.status_code}'
-    
+
+    get_updated_response = get_debt_position(subscription_key, organization_id, iupd)
+    assert get_updated_response.status_code == 200, f'Expected 200 but got {get_updated_response.status_code}'
+    updated_data = get_updated_response.json()
+    updated_description = updated_data['paymentOption'][0]['description']
+    updated_amount = updated_data['paymentOption'][0]['amount']
+
+    assert initial_description != updated_description, 'Description was not updated'
+    assert initial_amount != updated_amount, 'Amount was not updated'
+    assert updated_description == 'Canone Unico Patrimoniale - CORPORATE Updated', "Description doesn't match expected value"
+    assert updated_amount == 9500, "Amount doesn't match expected value"
+
 
 @allure.feature('Debt Positions')
 @allure.story('Update Debt Position in DEV')
@@ -223,9 +241,9 @@ def test_update_debt_position_dev_happy_path():
         client_secret=secrets.debtor_service_provider.client_secret,
         access_token_function=get_access_token
     )
-    
+
     debtor_fc = fake_fc()
-    
+
     activation_response = activate(
         access_token,
         debtor_fc,
@@ -243,11 +261,26 @@ def test_update_debt_position_dev_happy_path():
 
     create_response = create_debt_position_dev(subscription_key, organization_id, payload, to_publish=True)
     assert create_response.status_code == 201, f'Expected 201 but got {create_response.status_code}'
-    print(f"Create response body: {create_response.json()}")
-    
+
+    get_initial_response = get_debt_position_dev(subscription_key, organization_id, iupd)
+    assert get_initial_response.status_code == 200, f'Expected 200 but got {get_initial_response.status_code}'
+    initial_data = get_initial_response.json()
+    initial_description = initial_data['paymentOption'][0]['description']
+    initial_amount = initial_data['paymentOption'][0]['amount']
+
     time.sleep(1)
-    
+
     update_payload = create_debt_position_update_payload(iupd=iupd, debtor_fc=debtor_fc, iuv=iuv)
-    
     update_response = update_debt_position_dev(subscription_key, organization_id, iupd, update_payload)
     assert update_response.status_code == 200, f'Expected 200 but got {update_response.status_code}'
+
+    get_updated_response = get_debt_position_dev(subscription_key, organization_id, iupd)
+    assert get_updated_response.status_code == 200, f'Expected 200 but got {get_updated_response.status_code}'
+    updated_data = get_updated_response.json()
+    updated_description = updated_data['paymentOption'][0]['description']
+    updated_amount = updated_data['paymentOption'][0]['amount']
+
+    assert initial_description != updated_description, 'Description was not updated'
+    assert initial_amount != updated_amount, 'Amount was not updated'
+    assert updated_description == 'Canone Unico Patrimoniale - CORPORATE Updated', "Description doesn't match expected value"
+    assert updated_amount == 9500, "Amount doesn't match expected value"
--- utils/dataset.py
+++ utils/dataset.py
@@ -458,59 +458,59 @@ def create_debt_position_update_payload(iupd, debtor_fc=None, iuv=None):
     retention_date = now + timedelta(days=70)
     validity_date = now - timedelta(days=4)
 
-    date_format = "%Y-%m-%dT%H:%M:%S.000"
+    date_format = '%Y-%m-%dT%H:%M:%S.000'
     due_date_str = due_date.astimezone(timezone.utc).strftime(date_format)
     retention_date_str = retention_date.astimezone(timezone.utc).strftime(date_format)
     validity_date_str = validity_date.astimezone(timezone.utc).strftime(date_format)
 
     return {
-        "iupd": iupd,
-        "type": "F",
-        "fiscalCode": debtor_fc,
-        "fullName": "John Doe",
-        "streetName": "streetName",
-        "civicNumber": "11",
-        "postalCode": "00100",
-        "city": "city",
-        "province": "RM",
-        "region": "RM",
-        "country": "IT",
-        "email": "lorem@lorem.com",
-        "phone": "333-987654321",
-        "companyName": "companyName",
-        "officeName": "officeName",
-        "switchToExpired": False,
-        "validityDate": validity_date_str,
-        "paymentOption": [
+        'iupd': iupd,
+        'type': 'F',
+        'fiscalCode': debtor_fc,
+        'fullName': 'John Doe',
+        'streetName': 'streetName',
+        'civicNumber': '11',
+        'postalCode': '00100',
+        'city': 'city',
+        'province': 'RM',
+        'region': 'RM',
+        'country': 'IT',
+        'email': 'lorem@lorem.com',
+        'phone': '333-987654321',
+        'companyName': 'companyName',
+        'officeName': 'officeName',
+        'switchToExpired': False,
+        'validityDate': validity_date_str,
+        'paymentOption': [
             {
-                "iuv": iuv,
-                "amount": 9500,
-                "description": "Canone Unico Patrimoniale - CORPORATE Updated",
-                "isPartialPayment": False,
-                "dueDate": due_date_str,
-                "retentionDate": retention_date_str,
-                "fee": 0,
-                "transfer": [
+                'iuv': iuv,
+                'amount': 9500,
+                'description': 'Canone Unico Patrimoniale - CORPORATE Updated',
+                'isPartialPayment': False,
+                'dueDate': due_date_str,
+                'retentionDate': retention_date_str,
+                'fee': 0,
+                'transfer': [
                     {
-                        "idTransfer": "1",
-                        "amount": 8000,
-                        "remittanceInformation": "remittanceInformation 1",
-                        "category": "9/0101108TS/",
-                        "iban": "IT0000000000000000000000000"
+                        'idTransfer': '1',
+                        'amount': 8000,
+                        'remittanceInformation': 'remittanceInformation 1',
+                        'category': '9/0101108TS/',
+                        'iban': 'IT0000000000000000000000000'
                     },
                     {
-                        "idTransfer": "2",
-                        "amount": 1000,
-                        "remittanceInformation": "remittanceInformation 2",
-                        "category": "9/0101108TS/",
-                        "iban": "IT0000000000000000000000000"
+                        'idTransfer': '2',
+                        'amount': 1000,
+                        'remittanceInformation': 'remittanceInformation 2',
+                        'category': '9/0101108TS/',
+                        'iban': 'IT0000000000000000000000000'
                     },
                     {
-                        "idTransfer": "3",
-                        "amount": 500,
-                        "remittanceInformation": "remittanceInformation 3",
-                        "category": "9/0101108TS/",
-                        "iban": "IT0000000000000000000000000"
+                        'idTransfer': '3',
+                        'amount': 500,
+                        'remittanceInformation': 'remittanceInformation 3',
+                        'category': '9/0101108TS/',
+                        'iban': 'IT0000000000000000000000000'
                     }
                 ]
             }
