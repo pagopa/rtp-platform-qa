0a564c5f3c1f338aa3950e3755da4dc1d53fc904
Michele Fucito <michele.fucito@digitouch.it>
2025-07-24 14:21:24 +0200

    feat [SRTP-607]: remove get by notice number

--- api/debt_position.py
+++ api/debt_position.py
@@ -9,11 +9,7 @@ DEBT_POSITIONS_URL = config.debt_positions_base_url_path + config.debt_positions
 DEBT_POSITIONS_DEV_URL = (
     config.debt_positions_dev_base_url_path + config.debt_positions_dev_path
 )
-GET_BY_NOTICE_NUMBER_URL = (
-    config.rtp_creation_base_url_path.rstrip('/')
-    + config.send_rtp_path.lstrip('/')
-    + config.debt_positions_get_by_notice_number_path
-)
+
 
 
 def create_debt_position(
@@ -47,25 +43,3 @@ def create_debt_position_dev(
         timeout=config.default_timeout,
     )
 
-
-def get_debt_positions_by_notice_number(
-    notice_number: str, access_token: str
-) -> requests.Response:
-    """
-    Retrieve RTPs via noticeNumber (iuv)
-    """
-
-    url = GET_BY_NOTICE_NUMBER_URL
-
-    auth_header = (
-        access_token if access_token.startswith('Bearer ') else f"Bearer {access_token}"
-    )
-    headers = {
-        'Version': 'v1',
-        'RequestId': str(uuid.uuid4()),
-        'Authorization': auth_header,
-    }
-
-    response = requests.get(url, headers=headers, timeout=config.default_timeout)
-
-    return response
