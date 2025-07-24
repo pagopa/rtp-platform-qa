5d5263b5eacd7bd7be865297565d042e443ca8ff
Michele Fucito <michele.fucito@digitouch.it>
2025-07-24 11:09:51 +0200

    feat [SRTP-428]: get rtp status from db from after callback

--- utils/dataset.py
+++ utils/dataset.py
@@ -265,14 +265,14 @@ def generate_callback_data_DS_04b_compliant(BIC: str = 'MOCKSP04') -> dict:
     }
 
 
-def generate_callback_data_DS_08P_compliant(BIC: str = 'MOCKSP04') -> dict:
-    """Generate DS-08P compliant callback data.
+def generate_callback_data_DS_08N_compliant(BIC: str = 'MOCKSP04') -> dict:
+    """Generate DS-08N compliant callback data.
 
     Args:
         BIC: Bank Identifier Code
 
     Returns:
-        Dictionary containing DS-08P compliant callback data
+        Dictionary containing DS-08N compliant callback data
     """
     message_id = str(uuid.uuid4())
     resource_id = f"TestRtpMessage{generate_random_string(16)}"
@@ -354,4 +354,255 @@ def generate_callback_data_DS_08P_compliant(BIC: str = 'MOCKSP04') -> dict:
                 }
             },
         },
+    }
+
+def generate_callback_data_DS_05_ACTC_compliant(BIC: str = 'MOCKSP04') -> dict:
+    """Generate DS-05 compliant callback data.
+
+    Args:
+        BIC: Bank Identifier Code
+
+    Returns:
+        Dictionary containing DS-05 compliant callback data
+    """
+    message_id = str(uuid.uuid4())
+    resource_id = f"TestRtpMessage{generate_random_string(16)}"
+    original_msg_id = f"TestRtpMessage{generate_random_string(20)}"
+    transaction_id = generate_transaction_id()
+
+    create_time = generate_create_time()
+    original_time = generate_future_time(1)
+
+    # amount = random.randint(0, 999999999)
+    amount = round(random.uniform(1, 999999), 2)
+    expiry_date = generate_expiry_date(1, 30)
+    execution_date = generate_execution_date(1, 15)
+
+    return {
+        'resourceId': resource_id,
+        'AsynchronousSepaRequestToPayResponse': {
+            'resourceId': resource_id,
+            'Document': {
+                'CdtrPmtActvtnReqStsRpt': {
+                    'GrpHdr': {
+                        'MsgId': message_id,
+                        'CreDtTm': create_time,
+                        'InitgPty': {'Id': {'OrgId': {'AnyBIC': BIC}}},
+                    },
+                    'OrgnlGrpInfAndSts': {
+                        'OrgnlMsgId': original_msg_id,
+                        'OrgnlMsgNmId': 'pain.013.001.07',
+                        'OrgnlCreDtTm': original_time,
+                    },
+                    'OrgnlPmtInfAndSts': [
+                        {
+                            'OrgnlPmtInfId': str(uuid.uuid4()),
+                            'TxInfAndSts': {
+                                'StsId': message_id,
+                                'OrgnlInstrId': f"TestRtpMessage{generate_random_string(20)}",
+                                'OrgnlEndToEndId': generate_random_digits(18),
+                                'TxSts': 'ACTC',
+                                'StsRsnInf': {
+                                    'Orgtr': {'Id': {'OrgId': {'AnyBIC': BIC}}}
+                                },
+                                'OrgnlTxRef': {
+                                    'PmtTpInf': {
+                                        'SvcLvl': {'Cd': 'SRTP'},
+                                        'LclInstrm': {'Prtry': 'NOTPROVIDED'},
+                                    },
+                                    'RmtInf': {'Ustrd': fake.sentence()},
+                                    'Cdtr': {
+                                        'Id': {
+                                            'OrgId': {
+                                                'Othr': {
+                                                    'Id': transaction_id,
+                                                    'SchmeNm': {'Cd': 'BOID'},
+                                                }
+                                            }
+                                        },
+                                        'Nm': fake.company(),
+                                    },
+                                    'Dbtr': {
+                                        'Id': {
+                                            'PrvtId': {
+                                                'Othr': {
+                                                    'Id': transaction_id,
+                                                    'SchmeNm': {'Cd': 'POID'},
+                                                }
+                                            }
+                                        }
+                                    },
+                                    'DbtrAgt': {'FinInstnId': {'BICFI': BIC}},
+                                    'CdtrAgt': {'FinInstnId': {'BICFI': BIC}},
+                                    'CdtrAcct': {'Id': {'IBAN': generate_sepa_iban()}},
+                                    'Amt': {'InstdAmt': amount},
+                                    'ReqdExctnDt': {'Dt': f"{execution_date}Z"},
+                                    'XpryDt': {'Dt': f"{expiry_date}Z"},
+                                },
+                            },
+                        }
+                    ],
+                }
+            },
+        },
+    }
+
+
+def generate_iupd():
+    """
+    Generate a unique IUPD (Identificativo Univoco Posizione Debitoria).
+
+    Returns:
+        str: A unique identifier using UUID4 in hexadecimal format
+    """
+    return uuid.uuid4().hex[:17]
+
+def generate_iuv():
+    """
+    Generate a unique IUV (Identificativo Univoco Versamento).
+
+    Returns:
+        str: A random 18-digit number as string
+    """
+    return ''.join(random.choices('0123456789', k=18))
+
+def create_debt_position_payload(debtor_fc=None, iupd=None, iuv=None):
+    """
+    Generate a payload for debt position creation.
+
+    Args:
+        debtor_fc (str, optional): Fiscal code of the debtor. If None, a new one is generated.
+        iupd (str, optional): IUPD. If None, a new UUID is generated.
+        iuv (str, optional): IUV. If None, a random 17-digit number is generated.
+
+    Returns:
+        dict: Debt position payload
+    """
+    if debtor_fc is None:
+        debtor_fc = fake_fc()
+
+    if iupd is None:
+        iupd = uuid.uuid4().hex
+
+    if iuv is None:
+        iuv = generate_notice_number()
+
+    now_utc = datetime.now(timezone.utc)
+    due_date = (now_utc + timedelta(minutes=5)).strftime('%Y-%m-%dT%H:%M:%S.000Z')
+    retention_date = (now_utc + timedelta(days=60)).strftime('%Y-%m-%dT%H:%M:%S.000Z')
+    org_fc = secrets.debt_positions.organization_id
+    payload = {
+        'iupd': iupd,
+        'type': 'F',
+        'fiscalCode': debtor_fc,
+        'fullName': 'John Doe',
+        'streetName': 'streetName',
+        'civicNumber': '11',
+        'postalCode': '00100',
+        'city': 'city',
+        'province': 'RM',
+        'region': 'RM',
+        'country': 'IT',
+        'email': 'lorem@lorem.com',
+        'phone': '333-123456789',
+        'companyName': 'companyName',
+        'officeName': 'officeName',
+        'switchToExpired': False,
+        'pspCode': 'MOCKSP04',
+        'paymentOption': [
+            {
+                'iuv': iuv,
+                'amount': 10000,
+                'description': 'Canone Unico Patrimoniale - CORPORATE - TEST',
+                'isPartialPayment': False,
+                'dueDate': due_date,
+                'retentionDate': retention_date,
+                'fee': 0,
+                'organizationFiscalCode': org_fc,
+                'transfer': [
+                    {
+                        'idTransfer': '1',
+                        'amount': 10000,
+                        'remittanceInformation': 'remittanceInformation 1',
+                        'category': '9/0201102IM/',
+                        'organizationFiscalCode': org_fc,
+                        'iban': 'IT0000000000000000000000000000'
+                    }
+                ]
+            }
+        ]
+    }
+    return payload
+
+def create_debt_position_update_payload(iupd, debtor_fc=None, iuv=None):
+    """
+    Generate a payload for debt position update.
+    """
+    if debtor_fc is None:
+        debtor_fc = fake_fc()
+    if iuv is None:
+        iuv = generate_iuv()
+
+    now = datetime.now(timezone.utc)
+    due_date = now + timedelta(days=7)
+    retention_date = now + timedelta(days=70)
+    validity_date = now - timedelta(days=4)
+
+    date_format = "%Y-%m-%dT%H:%M:%S.000"
+    due_date_str = due_date.astimezone(timezone.utc).strftime(date_format)
+    retention_date_str = retention_date.astimezone(timezone.utc).strftime(date_format)
+    validity_date_str = validity_date.astimezone(timezone.utc).strftime(date_format)
+
+    return {
+        "iupd": iupd,
+        "type": "F",
+        "fiscalCode": debtor_fc,
+        "fullName": "John Doe",
+        "streetName": "streetName",
+        "civicNumber": "11",
+        "postalCode": "00100",
+        "city": "city",
+        "province": "RM",
+        "region": "RM",
+        "country": "IT",
+        "email": "lorem@lorem.com",
+        "phone": "333-987654321",
+        "companyName": "companyName",
+        "officeName": "officeName",
+        "switchToExpired": False,
+        "validityDate": validity_date_str,
+        "paymentOption": [
+            {
+                "iuv": iuv,
+                "amount": 9500,
+                "description": "Canone Unico Patrimoniale - CORPORATE Updated",
+                "isPartialPayment": False,
+                "dueDate": due_date_str,
+                "retentionDate": retention_date_str,
+                "fee": 0,
+                "transfer": [
+                    {
+                        "idTransfer": "1",
+                        "amount": 8000,
+                        "remittanceInformation": "remittanceInformation 1",
+                        "category": "9/0101108TS/",
+                        "iban": "IT0000000000000000000000000"
+                    },
+                    {
+                        "idTransfer": "2",
+                        "amount": 1000,
+                        "remittanceInformation": "remittanceInformation 2",
+                        "category": "9/0101108TS/",
+                        "iban": "IT0000000000000000000000000"
+                    },
+                    {
+                        "idTransfer": "3",
+                        "amount": 500,
+                        "remittanceInformation": "remittanceInformation 3",
+                        "category": "9/0101108TS/",
+                        "iban": "IT0000000000000000000000000"
+                    }
+                ]
+            }
+        ]
     }
