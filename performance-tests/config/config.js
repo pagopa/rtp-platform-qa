7ae1885c9875e9e7733ada9f739a3b81199b9674
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-07-30 16:06:36 +0200

    refactor: [SRTP-722] move access token url into constant in performance-tests config

--- performance-tests/config/config.js
+++ performance-tests/config/config.js
@@ -1,4 +1,6 @@
+const ACCESS_TOKEN_URL = 'https://api-mcshared.uat.cstar.pagopa.it/auth/token';
+
 export const activationConfig = {
-  access_token_url: 'https://api-mcshared.uat.cstar.pagopa.it/auth/token',
+  access_token_url: ACCESS_TOKEN_URL,
   activation_base: 'https://api-rtp.uat.cstar.pagopa.it/rtp/activation'
 };
