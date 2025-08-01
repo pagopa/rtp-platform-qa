8d2f1845526317045f924e2a8ee1c44c7e800e3b
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-08-01 09:48:39 +0200

    SRTP-769-implement-api-send-gpd-message

--- gpd-test/keyvault.py
+++ gpd-test/keyvault.py
@@ -1,19 +1,3 @@
-from azure.identity import AzureCliCredential
-from azure.keyvault.secrets import SecretClient
-import os
-
-def get_eventhub_connection_string():
-    keyvault_name = os.environ["KEYVAULT_NAME"]
-    secret_name = os.environ["EVENTHUB_SECRET_NAME"]
-
-    keyvault_url = f"https://{keyvault_name}.vault.azure.net"
-    credential = AzureCliCredential()
-    client = SecretClient(vault_url=keyvault_url, credential=credential)
-
-    secret = client.get_secret(secret_name)
-    print(f"[KeyVault] Retrieved secret '{secret_name}' from '{keyvault_name}'")
-    return secret.value
-
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 import os
