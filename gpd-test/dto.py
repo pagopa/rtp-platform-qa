2ade3c34e3f540be3d7334261d41cc342baf8fd5
Luca Consalvi <117908483+lucaconsalvi@users.noreply.github.com>
2025-09-01 11:38:16 +0200

    chore: [SRTP-825] Test cancel (#83)
    * [SRTP-825]: Change dto gpd test api to accept none for status, add cancel rtp from queue.
    
    * [SRTP-825]: Update dto.
    
    * [SRTP-825]: Add generation function to have a query to use inside mongo compass.

--- gpd-test/dto.py
+++ gpd-test/dto.py
@@ -20,15 +20,15 @@ class RTPMessage(BaseModel):
   id: int
   operation: RTPOperationCode
   timestamp: int
-  iuv: str
-  subject: str
-  description: str
-  ec_tax_code: str
-  debtor_tax_code: str
-  nav: str
-  due_date: int
-  amount: int
-  status: PaymentPositionStatus
+  iuv: Optional[str] = None
+  subject: Optional[str] = None
+  description: Optional[str] = None
+  ec_tax_code: Optional[str] = None
+  debtor_tax_code: Optional[str] = None
+  nav: Optional[str] = None
+  due_date: Optional[int] = None
+  amount: Optional[int] = None
+  status: Optional[PaymentPositionStatus] = None
   psp_code: Optional[str] = None
   psp_tax_code: Optional[str] = None
   is_partial_payment: Optional[bool] = None
