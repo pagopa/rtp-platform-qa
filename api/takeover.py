223589938e546a7d06ffb87cf4cb660a668aa26e
Michele Fucito <michele.fucito@digitouch.it>
2025-11-26 09:05:09 +0100

    fix: [SRTP-1168] payload has to be optional for testing purpose in takeover

--- api/takeover.py
+++ api/takeover.py
@@ -5,13 +5,20 @@ import requests
 
 from config.configuration import config
 
-def takeover_activation(access_token: str, payer_fiscal_code: str, service_provider_id: str, otp: str) -> requests.Response:
+def takeover_activation(
+    access_token: str,
+    payer_fiscal_code: str,
+    service_provider_id: str,
+    otp: str,
+    include_payload: bool = False,
+) -> requests.Response:
     """API to takeover an activation from one service provider to another
 
     :param access_token: The authentication token
     :param payer_fiscal_code: The fiscal code of the payer
     :param service_provider_id: The new service provider ID to takeover with
     :param otp: One-Time Password for verification (required)
+    :param include_payload: If True, send body payload with payer data
     :returns: the response of the call.
     :rtype: requests.Response
     """
@@ -27,12 +34,14 @@ def takeover_activation(access_token: str, payer_fiscal_code: str, service_provi
         'Content-Type': 'application/json'
     }
 
-    payload = {
-        'payer': {
-            'fiscalCode': payer_fiscal_code,
-            'rtpSpId': service_provider_id
+    payload = None
+    if include_payload:
+        payload = {
+            'payer': {
+                'fiscalCode': payer_fiscal_code,
+                'rtpSpId': service_provider_id
+            }
         }
-    }
 
     response = requests.post(
         url=url,
