b890387bb06ce2a3f72b610234bd9cdeef69c0ca
Michele Fucito <michele.fucito@digitouch.it>
2025-12-03 11:07:24 +0100

    feat: [SRTP-1202] use fixture in cancel RTP feature

--- functional-tests/tests/test_cancel_rtp.py
+++ functional-tests/tests/test_cancel_rtp.py
@@ -16,21 +16,13 @@ from utils.dataset import generate_rtp_data
 @allure.title('RTP is successfully cancelled')
 @pytest.mark.cancel
 @pytest.mark.happy_path
-def test_cancel_rtp_success():
+def test_cancel_rtp_success(creditor_service_provider_token_a, activate_payer):
+    
     rtp_data = generate_rtp_data()
+    creditor_service_provider_access_token = creditor_service_provider_token_a
 
-    debtor_service_provider_access_token = get_valid_access_token(
-        client_id=secrets.debtor_service_provider.client_id,
-        client_secret=secrets.debtor_service_provider.client_secret,
-        access_token_function=get_access_token)
+    activation_response = activate_payer(rtp_data['payer']['payerId'])
 
-    creditor_service_provider_access_token = get_valid_access_token(
-        client_id=secrets.creditor_service_provider.client_id,
-        client_secret=secrets.creditor_service_provider.client_secret,
-        access_token_function=get_access_token)
-
-    activation_response = activate(debtor_service_provider_access_token, rtp_data['payer']['payerId'],
-                                   secrets.debtor_service_provider.service_provider_id)
     assert activation_response.status_code == 201, 'Error activating debtor'
 
     send_response = send_rtp(access_token=creditor_service_provider_access_token, rtp_payload=rtp_data)
@@ -48,12 +40,9 @@ def test_cancel_rtp_success():
 @allure.title('RTP cancellation fails if ID does not exist')
 @pytest.mark.cancel
 @pytest.mark.unhappy_path
-def test_cancel_rtp_with_nonexistent_resource_id():
-    creditor_service_provider_access_token = get_valid_access_token(
-        client_id=secrets.creditor_service_provider.client_id,
-        client_secret=secrets.creditor_service_provider.client_secret,
-        access_token_function=get_access_token)
-
+def test_cancel_rtp_with_nonexistent_resource_id(creditor_service_provider_token_a):
+    
+    creditor_service_provider_access_token = creditor_service_provider_token_a
     fake_resource_id = str(uuid.uuid4())
     cancel_response = cancel_rtp(creditor_service_provider_access_token, fake_resource_id)
     assert cancel_response.status_code == 404, 'Expected 404 Not Found for non-existent resource'
