6eb06b2a0edb63473620fcd992ee8f82a96850e5
Michele Fucito <michele.fucito@digitouch.it>
2025-12-05 11:26:03 +0100

    feat: [SRTP-1215] centralize bdd logic

--- bdd-tests/steps/dataset_steps.py
+++ bdd-tests/steps/dataset_steps.py
@@ -2,13 +2,13 @@ from behave import given
 
 
 @given('the debtor {debtor_name} has fiscal code {debtor_fc}')
-def step_citizen_fc_exact_or_random(context, debtor_name, debtor_fc, random_fiscal_code):
+def step_citizen_fc_exact_or_random(context, debtor_name, debtor_fc):
     if debtor_fc == 'random':
-        debtor_fc = random_fiscal_code
+        debtor_fc = context.fake_fc()
 
     context.latest_citizen_fc = debtor_fc
 
-    try:
-        context.debtor_fc[debtor_name] = debtor_fc
-    except AttributeError:
-        context.debtor_fc = {debtor_name: debtor_fc}
+    if not hasattr(context, 'debtor_fc') or context.debtor_fc is None:
+        context.debtor_fc = {}
+
+    context.debtor_fc[debtor_name] = debtor_fc
