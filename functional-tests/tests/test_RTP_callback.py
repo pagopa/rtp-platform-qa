d81d6be99d30980f121040cd7cd9fa47952a0b10
Michele Fucito <michele.fucito@digitouch.it>
2025-12-17 15:20:24 +0100

    feat: [SRTP-1256] add callback with status RJCT

--- functional-tests/tests/test_RTP_callback.py
+++ functional-tests/tests/test_RTP_callback.py
@@ -8,7 +8,8 @@ from utils.callback_builder import build_callback_with_original_msg_id
 from utils.dataset_callback_data_DS_04b_compliant import generate_callback_data_DS_04b_compliant
 from utils.dataset_callback_data_DS_05_ACTC_compliant import generate_callback_data_DS_05_ACTC_compliant
 from utils.dataset_callback_data_DS_08N_compliant import generate_callback_data_DS_08N_compliant
-from utils.dataset_callback_data_DS_08P_compliant import generate_callback_data_DS_08P_compliant
+from utils.dataset_callback_data_DS_08P_ACCP_compliant import generate_callback_data_DS_08P_ACCP_compliant
+from utils.dataset_callback_data_DS_08P_RJCT_compliant import generate_callback_data_DS_08P_RJCT_compliant
 from utils.dataset_RTP_data import generate_rtp_data
 
 @allure.epic('RTP Callback')
@@ -323,7 +324,7 @@ def test_receive_rtp_callback_DS_08P_compliant(
     assert body['status'] == 'ACCEPTED'
 
     callback_data = build_callback_with_original_msg_id(
-        generate_callback_data_DS_08P_compliant,
+        generate_callback_data_DS_08P_ACCP_compliant,
         original_msg_id,
         is_document=True,
     )
@@ -345,3 +346,161 @@ def test_receive_rtp_callback_DS_08P_compliant(
     assert get_response.status_code == 200
     body = get_response.json()
     assert body['status'] == 'USER_ACCEPTED'
+
+
+@allure.epic('RTP Callback')
+@allure.feature('RTP Callback')
+@allure.story('Service provider sends a callback referred to an RTP')
+@allure.title('An RTP callback is successfully received with status ACCP')
+@allure.tag('functional', 'happy_path', 'rtp_callback', 'DS_08P_compliant')
+@pytest.mark.callback
+@pytest.mark.happy_path
+def test_receive_rtp_callback_DS_08P_compliant_ACCP(
+    creditor_service_provider_token_a,
+    rtp_reader_access_token,
+    activate_payer,
+    debtor_sp_mock_cert_key,
+):
+
+    rtp_data = generate_rtp_data()
+
+    activation_response = activate_payer(rtp_data['payer']['payerId'])
+    assert activation_response.status_code == 201
+
+    send_response = send_rtp(
+        access_token=creditor_service_provider_token_a,
+        rtp_payload=rtp_data,
+    )
+    assert send_response.status_code == 201
+
+    location = send_response.headers['Location']
+    resource_id = location.split('/')[-1]
+    original_msg_id = resource_id.replace('-', '')
+
+    callback_data = build_callback_with_original_msg_id(
+        generate_callback_data_DS_05_ACTC_compliant,
+        original_msg_id,
+        is_document=True,
+    )
+
+    cert, key = debtor_sp_mock_cert_key
+
+    callback_response = srtp_callback(
+        rtp_payload=callback_data,
+        cert_path=cert,
+        key_path=key,
+    )
+    assert (
+        callback_response.status_code == 200
+    ), f"Error from callback, expected 200 got {callback_response.status_code}"
+
+    get_response = get_rtp(
+        access_token=rtp_reader_access_token,
+        rtp_id=resource_id,
+    )
+    assert get_response.status_code == 200
+    body = get_response.json()
+    assert body['status'] == 'ACCEPTED'
+
+    callback_data = build_callback_with_original_msg_id(
+        generate_callback_data_DS_08P_ACCP_compliant,
+        original_msg_id,
+        is_document=True,
+    )
+
+    cert, key = debtor_sp_mock_cert_key
+    callback_response = srtp_callback(
+        rtp_payload=callback_data,
+        cert_path=cert,
+        key_path=key,
+    )
+    assert (
+        callback_response.status_code == 200
+    ), f"Error from callback, expected 200 got {callback_response.status_code}"
+
+    get_response = get_rtp(
+        access_token=rtp_reader_access_token,
+        rtp_id=resource_id,
+    )
+    assert get_response.status_code == 200
+    body = get_response.json()
+    assert body['status'] == 'USER_ACCEPTED'
+
+
+@allure.epic('RTP Callback')
+@allure.feature('RTP Callback')
+@allure.story('Service provider sends a callback referred to an RTP')
+@allure.title('An RTP callback is successfully received with status RJCT')
+@allure.tag('functional', 'happy_path', 'rtp_callback', 'DS_08P_compliant')
+@pytest.mark.callback
+@pytest.mark.happy_path
+def test_receive_rtp_callback_DS_08P_compliant_RJCT(
+    creditor_service_provider_token_a,
+    rtp_reader_access_token,
+    activate_payer,
+    debtor_sp_mock_cert_key,
+):
+
+    rtp_data = generate_rtp_data()
+
+    activation_response = activate_payer(rtp_data['payer']['payerId'])
+    assert activation_response.status_code == 201
+
+    send_response = send_rtp(
+        access_token=creditor_service_provider_token_a,
+        rtp_payload=rtp_data,
+    )
+    assert send_response.status_code == 201
+
+    location = send_response.headers['Location']
+    resource_id = location.split('/')[-1]
+    original_msg_id = resource_id.replace('-', '')
+
+    callback_data = build_callback_with_original_msg_id(
+        generate_callback_data_DS_05_ACTC_compliant,
+        original_msg_id,
+        is_document=True,
+    )
+
+    cert, key = debtor_sp_mock_cert_key
+
+    callback_response = srtp_callback(
+        rtp_payload=callback_data,
+        cert_path=cert,
+        key_path=key,
+    )
+    assert (
+        callback_response.status_code == 200
+    ), f"Error from callback, expected 200 got {callback_response.status_code}"
+
+    get_response = get_rtp(
+        access_token=rtp_reader_access_token,
+        rtp_id=resource_id,
+    )
+    assert get_response.status_code == 200
+    body = get_response.json()
+    assert body['status'] == 'ACCEPTED'
+
+    callback_data = build_callback_with_original_msg_id(
+        generate_callback_data_DS_08P_RJCT_compliant,
+        original_msg_id,
+        is_document=True,
+    )
+
+    cert, key = debtor_sp_mock_cert_key
+    callback_response = srtp_callback(
+        rtp_payload=callback_data,
+        cert_path=cert,
+        key_path=key,
+    )
+    assert (
+        callback_response.status_code == 200
+    ), f"Error from callback, expected 200 got {callback_response.status_code}"
+
+    get_response = get_rtp(
+        access_token=rtp_reader_access_token,
+        rtp_id=resource_id,
+    )
+    assert get_response.status_code == 200
+    body = get_response.json()
+    assert body['status'] == 'USER_REJECTED'
