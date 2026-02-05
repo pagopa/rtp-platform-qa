c661b2fc3640611501b9e9cbacddb00b2ee408da
Francesco Muscianisi <109276448+fmuscianisi@users.noreply.github.com>
2026-02-05 11:49:34 +0100

    feat: [SRTP-1340] enrich Poste probe tests with OAuth2 token retrieval (#144)
    * feat: [SRTP-1340] add new JsonType hint
    
    * feat: [SRTP-1340] enrich Poste send API with Bearer token
    
    * feat: [SRTP-1340] enrich Poste probe happy case with bearer token
    
    * chore: [SRTP-1340] add missing type hint in 'test_get_poste_access_token'
    
    * feat: [SRTP-1340] enrich Poste probe with invalid amount case with Bearer token
    
    * feat: [SRTP-1340] enrich Poste probe with over limit amount case with Bearer token
    
    * feat: [SRTP-1340] enrich Poste probe with expiry date in past case with Bearer token
    
    * chore: [SRTP-1340] add type hinting to CBI probe API call
    
    * chore: [SRTP-1340] add docstring to CBI probe API call
    
    * chore: [SRTP-1340] remove unused import in Debtor Service Provider API module
    
    * chore: [SRTP-1340] add type hinting to ICCREA probe API call
    
    * chore: [SRTP-1340] add docstring to ICCREA probe API call
    
    * fix: [SRTP-1340] format access token correctly in Debtor Service Provider APIs
    
    * refactor: [SRTP-1340] remove explicit Bearer token enriching in CBI probe tests
    
    * chore: [SRTP-1340] rename variable to improve clarity
    
    ---------
    
    Co-authored-by: Francesco Muscianisi <francesco.muscianisi@pagopa.it>

--- api/debtor_service_provider_api.py
+++ api/debtor_service_provider_api.py
@@ -9,15 +9,25 @@ from api.utils.endpoints import POSTE_SEND_URL
 from api.utils.http_utils import CERT_PATH
 from api.utils.http_utils import HTTP_TIMEOUT
 from api.utils.http_utils import KEY_PATH
-from config.configuration import config
 from utils.idempotency_key_utils import generate_idempotency_key
+from utils.type_utils import JsonType
 
 
-def send_srtp_to_cbi(access_token: str, rtp_payload):
+def send_srtp_to_cbi(access_token: str, rtp_payload: JsonType):
+    """
+    Sends an RTP payload to the CBI endpoint.
+
+    :param access_token: Bearer access token for authorization.
+    :type access_token: str
+    :param rtp_payload: RTP payload in JSON format.
+    :type rtp_payload: JsonType
+    """
     idempotency_key = generate_idempotency_key(CREATE_RTP_OPERATION, rtp_payload['resourceId'])
+    bearer_token = _create_bearer_token(access_token)
+
     return requests.post(
         headers={
-            'Authorization': f'{access_token}',
+            'Authorization': f'{bearer_token}',
             'Idempotency-key': idempotency_key,
             'X-Request-ID': str(uuid.uuid4())
         },
@@ -28,10 +38,21 @@ def send_srtp_to_cbi(access_token: str, rtp_payload):
     )
 
 
-def send_srtp_to_poste(rtp_payload):
+def send_srtp_to_poste(access_token: str, rtp_payload: JsonType):
+    """
+    Sends an RTP payload to the POSTE endpoint.
+
+    :param access_token: Bearer access token for authorization.
+    :type access_token: str
+    :param rtp_payload: RTP payload in JSON format.
+    :type rtp_payload: JsonType
+    """
     idempotency_key = generate_idempotency_key(CREATE_RTP_OPERATION, rtp_payload['resourceId'])
+    bearer_token = _create_bearer_token(access_token)
+
     return requests.post(
         headers={
+            'Authorization': f'{bearer_token}',
             'Idempotency-key': idempotency_key,
             'X-Request-ID': str(uuid.uuid4())
         },
@@ -43,7 +64,13 @@ def send_srtp_to_poste(rtp_payload):
     )
 
 
-def send_srtp_to_iccrea(rtp_payload):
+def send_srtp_to_iccrea(rtp_payload: JsonType):
+    """
+    Sends an RTP payload to the ICCREA endpoint.
+
+    :param rtp_payload: RTP payload in JSON format.
+    :type rtp_payload: JsonType
+    """
     idempotency_key = generate_idempotency_key(CREATE_RTP_OPERATION, rtp_payload['resourceId'])
     return requests.post(
         headers={
@@ -55,3 +82,15 @@ def send_srtp_to_iccrea(rtp_payload):
         cert=(CERT_PATH, KEY_PATH),
         timeout=HTTP_TIMEOUT,
     )
+
+
+def _create_bearer_token(access_token: str) -> str:
+    """
+    Ensures that the access token is in the correct format for the Authorization header.
+
+    :param access_token: The raw access token.
+    :type access_token: str
+    :return: A properly formatted Bearer token.
+    :rtype: str
+    """
+    return f'Bearer {access_token}' if not access_token.startswith('Bearer ') else access_token
