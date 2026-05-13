ed12d16fe1d565288fc93f566a41dddc04708cfa
Michele Fucito <michele.fucito@digitouch.it>
2026-05-13 12:43:53 +0200

    refactor: [SRTP-1793] refactor cancel api to reflect new contract

--- api/RTP_cancel_api.py
+++ api/RTP_cancel_api.py
@@ -5,13 +5,20 @@ import requests
 from api.utils.api_version import CANCEL_VERSION
 from api.utils.endpoints import CANCEL_RTP_OPERATION, CANCEL_RTP_URL
 from api.utils.http_utils import HTTP_TIMEOUT
+from utils.idempotency_key_utils import generate_idempotency_key
 
-from .debtor_service_provider_api import generate_idempotency_key
 
+def cancel_rtp(access_token: str, resource_id: str, reason: str):
+    """
+    Cancel an RTP request.
 
-def cancel_rtp(access_token: str, rtp_id: str):
-    url = CANCEL_RTP_URL.format(rtp_id=rtp_id)
-    idempotency_key = generate_idempotency_key(CANCEL_RTP_OPERATION, rtp_id)
+    :param access_token: Bearer access token for authorization.
+    :param resource_id: UUID of the RTP resource to cancel.
+    :param reason: Cancellation reason. Must be one of: PAID, MODT.
+    :returns: The HTTP response.
+    :rtype: requests.Response
+    """
+    idempotency_key = generate_idempotency_key(CANCEL_RTP_OPERATION, resource_id)
 
     headers = {
         "Authorization": f"{access_token}",
@@ -20,4 +27,9 @@ def cancel_rtp(access_token: str, rtp_id: str):
         "Idempotency-key": idempotency_key,
     }
 
-    return requests.post(headers=headers, url=url, timeout=HTTP_TIMEOUT)
+    body = {
+        "resourceId": resource_id,
+        "reason": reason,
+    }
+
+    return requests.post(headers=headers, url=CANCEL_RTP_URL, json=body, timeout=HTTP_TIMEOUT)
