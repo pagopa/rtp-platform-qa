c80d2782756cd36223f92963dedcf1b2e93f2c1a
Michele Fucito <michele.fucito@digitouch.it>
2026-07-01 16:16:59 +0200

    refactor: [SRTP-1978] streamline RTP request handling by consolidating post logic into a single function

--- api/RTP_send_api.py
+++ api/RTP_send_api.py
@@ -4,30 +4,29 @@ from api.utils.endpoints import SEND_RTP_URL, SERVICE_PROVIDER_MOCK_URL
 from api.utils.http_utils import HTTP_TIMEOUT
 
 
-def send_rtp(access_token: str, rtp_payload):
-    """API to post a rtp request
-    :returns: the response of the call.
-    :rtype: requests.Response
-    """
+def _post_rtp(access_token: str, rtp_payload, version: str) -> requests.Response:
     return requests.post(
-        headers={"Authorization": f"{access_token}", "Version": "v1"},
+        headers={"Authorization": f"{access_token}", "Version": version},
         url=SEND_RTP_URL,
         json=rtp_payload,
         timeout=HTTP_TIMEOUT,
     )
+
+
+def send_rtp(access_token: str, rtp_payload):
+    """API to post a rtp request (Version: v1).
+    :returns: the response of the call.
+    :rtype: requests.Response
+    """
+    return _post_rtp(access_token, rtp_payload, "v1")
 
 
 def send_rtp_v2(access_token: str, rtp_payload):
-    """API to post a rtp request using Version v2 header.
+    """API to post a rtp request (Version: v2).
     :returns: the response of the call.
     :rtype: requests.Response
     """
-    return requests.post(
-        headers={"Authorization": f"{access_token}", "Version": "v2"},
-        url=SEND_RTP_URL,
-        json=rtp_payload,
-        timeout=HTTP_TIMEOUT,
-    )
+    return _post_rtp(access_token, rtp_payload, "v2")
 
 
 def send_rtp_to_mock(rtp_payload):
